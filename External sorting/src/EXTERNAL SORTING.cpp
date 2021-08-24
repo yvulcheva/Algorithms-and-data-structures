508d256a4bafab228eaa7f7ebf8d5421ce867d7b
yvulcheva <yvulcheva@gmail.com>
2021-08-24 20:07:40 +0300

    Added extra information

--- External sorting/src/EXTERNAL SORTING.cpp
+++ External sorting/src/EXTERNAL SORTING.cpp
@@ -1,44 +1,53 @@
-//Yolina Vulcheva - 62426
+//Yolina Valcheva
+//Structures and algorithms course assignment FMI
+//External sorting
 
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include<algorithm>
+#include <algorithm>
 #include <string>
 #include <queue>
 
 using namespace std;
 
-void createFiles(size_t bufferSize, string fileToBeSortedName, int& numberOfFilesCreated) {
+void createFiles(size_t bufferSize, string fileToBeSortedName, int &numberOfFilesCreated)
+{
 	vector<int> vecWithNumbers;
 	ifstream fileToBeSorted(fileToBeSortedName.c_str());
 
-	if (fileToBeSorted.is_open()) {
+	if (fileToBeSorted.is_open())
+	{
 		//output file
 		ofstream file;
 
 		bool endOfBigFile = false;
 
-		while (!endOfBigFile) {
+		while (!endOfBigFile)
+		{
 
-			while (vecWithNumbers.size() < bufferSize) {
+			while (vecWithNumbers.size() < bufferSize)
+			{
 				int number;
-				if (fileToBeSorted >> number) {
+				if (fileToBeSorted >> number)
+				{
 					vecWithNumbers.push_back(number);
 				}
-				else {
+				else
+				{
 					endOfBigFile = true;
 					break;
 				}
 			}
 
-			//to avoid creating empty file 
+			//to avoid creating empty file
 			if (vecWithNumbers.empty())
 				break;
 
 			//create file for output which name is F and the number of file that is being created
 			file.open("F" + to_string(numberOfFilesCreated + 1) + ".txt");
-			if (file.is_open()) {
+			if (file.is_open())
+			{
 				//sort vector
 				std::sort(vecWithNumbers.begin(), vecWithNumbers.end());
 
@@ -52,9 +61,8 @@ void createFiles(size_t bufferSize, string fileToBeSortedName, int& numberOfFile
 
 				vecWithNumbers.clear();
 			}
-			else 
+			else
 				perror("Error: not able to open file");
-			
 		}
 
 		fileToBeSorted.close();
@@ -63,39 +71,46 @@ void createFiles(size_t bufferSize, string fileToBeSortedName, int& numberOfFile
 		perror("Error: not able to open file");
 }
 
-string _merge_K(int bufferSize, int indexOfName, vector<string> fileNames) {
+string _merge_K(int bufferSize, int indexOfName, vector<string> fileNames)
+{
 	ofstream mergedFile;
 	string mergedName = "Merged" + to_string(indexOfName + 1) + ".txt";
 	mergedFile.open(mergedName);
 
-	if (mergedFile.is_open()) {
+	if (mergedFile.is_open())
+	{
 		//storing every first element from input files and index of files
 		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> buffer;
 
 		//vector with all temp files
 		std::vector<ifstream> file(bufferSize);
 
-		for (int i = 0; i < bufferSize; i++) {
+		for (int i = 0; i < bufferSize; i++)
+		{
 			file[i].open(fileNames[i]);
 		}
 
 		int num;
-		for (int i = 0; i < bufferSize; i++) {
+		for (int i = 0; i < bufferSize; i++)
+		{
 			file[i] >> num;
 			//push the element and index of the location file
 			buffer.push(make_pair(num, i));
 		}
 
-		while (!buffer.empty()) {
+		while (!buffer.empty())
+		{
 			pair<int, int> temp = buffer.top();
 			buffer.pop();
 			mergedFile << temp.first << ' ';
 
-			if (file[temp.second] >> num) {
+			if (file[temp.second] >> num)
+			{
 				temp.first = num;
 				buffer.push(temp);
 			}
-			else {
+			else
+			{
 				file[temp.second].close();
 				remove(fileNames[temp.second].c_str());
 			}
@@ -104,13 +119,14 @@ string _merge_K(int bufferSize, int indexOfName, vector<string> fileNames) {
 		mergedFile.close();
 	}
 
-	else 
+	else
 		perror("Error: not able to open file");
-	
+
 	return mergedName;
 }
 
-void merge(int filesCreated, int bufferSize,string nameOfOutputFile) {
+void merge(int filesCreated, int bufferSize, string nameOfOutputFile)
+{
 	//names of temp files
 	vector<string> fileNames;
 	for (int i = 0; i < filesCreated; i++)
@@ -125,7 +141,8 @@ void merge(int filesCreated, int bufferSize,string nameOfOutputFile) {
 	endIndex = (filesCreated < bufferSize) ? filesCreated : bufferSize - 1;
 
 	int nameIndex = 0;
-	while (fileNames.size() > 1) {
+	while (fileNames.size() > 1)
+	{
 		vector<string> filesToMerge(endIndex);
 		for (int i = 0; i < endIndex; i++)
 			filesToMerge[i] = fileNames[i];
@@ -149,24 +166,27 @@ void merge(int filesCreated, int bufferSize,string nameOfOutputFile) {
 
 	string newName = nameOfOutputFile;
 
-	if (rename(oldName.c_str(), newName.c_str()) == 0) {
+	if (rename(oldName.c_str(), newName.c_str()) == 0)
+	{
 		auto output = "Sorted file named: " + nameOfOutputFile;
 		puts(output.c_str());
 	}
-	else {
+	else
+	{
 		perror("Error");
 		cout << "Sorted file named: " + oldName << "\n";
 	}
 }
 
-void ExternalSort(int Buffer, string fileToBeSorted,string nameOfOutputFile) {
+void ExternalSort(int Buffer, string fileToBeSorted, string nameOfOutputFile)
+{
 	//counter of files we create depending on the buffer size
 	int numberOfFilesCreated = 0;
 	//buffer is equal to the amount of numbers we can store in one run
 	Buffer = (Buffer * 1024 * 1024) / 4;
 	createFiles(Buffer, fileToBeSorted, numberOfFilesCreated);
-	
-	merge(numberOfFilesCreated,Buffer,nameOfOutputFile);
+
+	merge(numberOfFilesCreated, Buffer, nameOfOutputFile);
 }
 
 int main()
